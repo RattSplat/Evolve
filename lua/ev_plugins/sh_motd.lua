679d07a607407b7455b912bc09aeb7bcd6e24528
generalwrex <bjdroot@gmail.com>
2012-05-31 19:34:00 -0700

    updated motd yet again

--- lua/ev_plugins/sh_motd.lua
+++ lua/ev_plugins/sh_motd.lua
@@ -1,85 +1,116 @@
-
-
 local PLUGIN = {}
 PLUGIN.Title = "MOTD"
 PLUGIN.Description = "Message of the Day."
-PLUGIN.Author = "Wrex"
+PLUGIN.Author = "Wrex,MadDog"
 PLUGIN.ChatCommand = "motd"
 PLUGIN.Usage = nil
 PLUGIN.Privileges = nil
+PLUGIN.AcceptCloseDelay = 6 --seconds
 
-if (SERVER) then 
+
+
+CreateConVar("ev_motd", "http://dl.dropbox.com/u/45247280/ev_motd.htm", _, FCVAR_REPLICATED | FCVAR_ARCHIVE ) --off by default. 1 = enable, or enter a url to load a page (example: ev_motd "http://www.yourwebsite.com")
+if (SERVER) then 	
+
 	if (file.Exists("ev_motd.txt","DATA")) then
 		resource.AddFile("data/ev_motd.txt")
-		print("Sending MOTD file To Clients.")
 	end
-end
 
-
-
-function PLUGIN:OpenMotd( ply )
-	if (SERVER) then
+	function PLUGIN:OpenMotd( ply )
 		ply:ConCommand("evolve_motd")
 	end
-end
 
-function PLUGIN:ReadMOTD(txt)
-	if CLIENT then
-	
-		if file.Exists("ev_motd.txt","DATA") then
-			txt = file.Read( "ev_motd.txt","DATA")
-		else
-			txt = "Welcome to the server, Enjoy your stay!"
-		end
-	end
-	
-	return txt
 end
-
 
 function PLUGIN:Call( ply, args )
-	self:OpenMotd( ply )
-
-end
-
-
-
-if (CLIENT) then
-	function PLUGIN:CreateMenu()
-
-		self.MotdPanel = vgui.Create("DFrame")
-		local w,h = ScrW() - 200,ScrH() - 200
-		self.MotdPanel:SetPos( 100,100 )
-		self.MotdPanel:SetSize( w,h )
-		self.MotdPanel:SetTitle( "MOTD" )
-		self.MotdPanel:SetVisible( false )
-		self.MotdPanel:SetDraggable( false )
-		self.MotdPanel:ShowCloseButton( true )
-		self.MotdPanel:SetDeleteOnClose( false )
-		self.MotdPanel:SetScreenLock( true )
-		self.MotdPanel:MakePopup()
-		
-		self.MotdBox = vgui.Create("HTML",self.MotdPanel)
-		self.MotdBox:StretchToParent( 4,25,4,4 )
-		self.MotdBox:SetHTML(self:ReadMOTD())
+	if (SERVER) then
+		self:OpenMotd( ply )
 	end
-	timer.Simple( 0.1, function() PLUGIN:CreateMenu() end)
-	
-	concommand.Add("evolve_motd",function(ply,cmd,args)
-		PLUGIN.MotdPanel:SetVisible( true )
-	end)
-	
-	concommand.Add("evolve_startmotd",function(ply,cmd,args)
-		PLUGIN.StartPanel:SetVisible( true )
-	end)
-	
 end
 
-function ShowMOTDOnSpawn( ply )
-	timer.Simple( 3, function() ply:ConCommand("evolve_motd") end)
+function PLUGIN:ReadMOTD()
+	if CLIENT then
+		if file.Exists("ev_motd.txt","DATA") then
+			return file.Read( "ev_motd.txt","DATA")
+		end
+	end
 end
+
+function PLUGIN:PlayerInitialSpawn( ply )
+	if (SERVER) then
+		timer.Simple(4, self.OpenMotd, self, ply )
+	end
+end
+
+
+
+function PLUGIN:CreateMenu()
 	
-hook.Add( "PlayerInitialSpawn", "playerInitialSpawn", ShowMOTDOnSpawn)
+	if (CLIENT) then
+		local w,h = ScrW() - 100,ScrH() - 100
+		
+
+		--the frame
+		self.MotdPanel = vgui.Create( "DFrame" )
+		self.MotdPanel:SetSize( w, h )
+		self.MotdPanel:Center()
+		self.MotdPanel:SetTitle( self.Title )
+		self.MotdPanel:SetDraggable( false )
+		self.MotdPanel:SetScreenLock( true )
+		self.MotdPanel:SetDeleteOnClose( false )
+		self.MotdPanel:SetVisible( true )
+		self.MotdPanel:MakePopup()
+
+		--ShowCloseButton is useless in the beta so these are needed for now
+		self.MotdPanel.btnClose:SetVisible(false)
+		self.MotdPanel.btnMaxim:SetVisible(false)
+		self.MotdPanel.btnMinim:SetVisible(false)
+
+		--the Accept button
+		--must agree to close, and the button is disabled for a few seconds to start with!
+		self.AcceptButton = vgui.Create( "DButton", self.MotdPanel )
+		self.AcceptButton:SetText( "I Agree to the Rules" )
+		self.AcceptButton.DoClick = function()
+			if !(LocalPlayer():IsAdmin() or LocalPlayer():IsSuperAdmin()) then RunConsoleCommand("say", "I agree to the rules.") end --dont make admins say they agree
+			self.MotdPanel:Close()
+			gui.EnableScreenClicker( false )
+		end
+		self.AcceptButton:SetSize( 150, 40 )
+		self.AcceptButton:SetDisabled( true )
+		self.AcceptButton:SetPos( (self.MotdPanel:GetWide() - self.AcceptButton:GetWide()) / 2, self.MotdPanel:GetTall() - self.AcceptButton:GetTall() - 10 )
+
+		--the MOTD
+		self.MotdBox = vgui.Create( "DHTML", self.MotdPanel )
+		self.MotdBox:SetSize( self.MotdPanel:GetWide() - 10, self.MotdPanel:GetTall() - self.AcceptButton:GetTall() - 50 )
+		self.MotdBox:SetPos( 5, 30 )
+
+		if (!GetConVarString("ev_motd"):find("://")) then
+			self.MotdBox:SetHTML( self:ReadMOTD() )
+		else
+			self.MotdBox:OpenURL( GetConVarString("ev_motd") .. "?" .. os.time() ) --appending time insures the file is never cached by the client
+		end
+
+		timer.Simple( self.AcceptCloseDelay, function()
+			PLUGIN.AcceptButton:SetDisabled( false )
+		end)
+	end
+end
 
 
+
+
+concommand.Add("evolve_motd",function( ply, cmd, args )
+	if CLIENT then
+		if PLUGIN.MotdPanel then
+			PLUGIN.MotdPanel:SetVisible( true )			
+		else	
+			PLUGIN:CreateMenu() 
+		end
+	end
+end)
+
+
+
+	
+	
 evolve:RegisterPlugin( PLUGIN )
